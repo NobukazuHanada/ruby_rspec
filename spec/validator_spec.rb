d0ac4aa61df83cf1c000909f828fcc69bfc57ac9
Nobukazu Hanada <tone.nobukazu@gmail.com>
2013-06-15 17:20:38 +0900

    describe 0 string add

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,8 +1,10 @@
 require "spec_helper"
 
 describe "validator" do
-  it{ validation("").should be_false }
-  
+  describe "0 string" do 
+    it{ validation("").should be_false }
+  end
+
   describe "3 strings" do
     it{ ["000","111"].each{|zip_code| validation(zip_code).should be_true} }
     it{ ["aaa"].each{|zip_code| validation(zip_code).should be_false} }
