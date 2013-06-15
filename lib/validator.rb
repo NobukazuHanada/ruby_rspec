3fca38f100591d3ef3120ab46a975efce1a689cf
Nobukazu Hanada <tone.nobukazu@gmail.com>
2013-06-15 18:53:26 +0900

    分岐を改善

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,5 +1,7 @@
 def validation zip_code
-  if (zip_code.length == 3 or zip_code.length == 7) and zip_code =~ /\d+/
+  length_true = [3,7].map{|i| zip_code.length == i }.any?
+
+  if length_true and zip_code =~ /\d+/
     return true
   end
   if zip_code =~ /\d{3}-\d{4}/
