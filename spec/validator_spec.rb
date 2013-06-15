fa143f93b8f43e40f240873ad07f715301d483de
Nobukazu Hanada <tone.nobukazu@gmail.com>
2013-06-15 16:53:50 +0900

    test-refactoring

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -2,7 +2,9 @@ require "spec_helper"
 
 describe "validator" do
   it{ validation("").should be_false }
-  it{ validation("000").should be_true }
-  it{ validation("111").should be_true }
-  it{ validation("aaa").should be_false }
+  
+  describe "3 strings" do
+    it{ ["000","111"].each{|zip_code| validation(zip_code).should be_true} }
+    it{ ["aaa"].each{|zip_code| validation(zip_code).should be_false} }
+  end
 end
