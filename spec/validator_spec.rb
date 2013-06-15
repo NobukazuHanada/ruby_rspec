b16431ffae81f56f44ccf3a9b8241261762e7a63
Kosuke Komiya <komiya@isoftware.jp>
2013-06-15 17:30:02 +0900

    7桁がとりあえず通ります

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -9,4 +9,8 @@ describe "validator" do
     ["000","111"].each{|zip_code| it{ validation(zip_code).should be_true } } 
     ["aaa"].each{|zip_code| it{ validation(zip_code).should be_false } } 
   end
+  
+  describe "7 strings" do
+    it{ validation("1111111").should be_true }
+  end
 end
