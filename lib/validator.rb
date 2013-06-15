a24d97591af7b24eb797e90a0ca6981993933db4
Kosuke Komiya <komiya@isoftware.jp>
2013-06-15 16:31:10 +0900

    ３桁が正しく通るようにした

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,5 +1,5 @@
 def validation zip_code
-  if "000" == zip_code
+  if zip_code.length == 3
     return true
   end
   false
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -3,4 +3,5 @@ require "spec_helper"
 describe "validator" do
   it{ validation("").should be_false }
   it{ validation("000").should be_true }
+  it{ validation("111").should be_true }
 end
